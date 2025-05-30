0e86a01d62a67ab345e5535ee904fd859c278cb7
Emeric C Alex <emericcalex@Emerics-MacBook-Air.local>
2025-05-30 23:59:38 +0530

    First version of car fiter project

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <a href="#">Logout</a>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'HeaderPage'
+   
+}
+</script>
+
+<style scoped>
+
+
+</style>
